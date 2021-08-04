5d5348a148d9a78b12d2db36c37e0b440f5a5941
Rogério Soares <rogerio_si@yahoo.com.br>
2021-08-04 17:58:57 +0000

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ X_FRAME_OPTIONS = '*'
 #]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
